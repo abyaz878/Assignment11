41b69cbb3505ef1464c71f16fc1284ba604c723c
abyaz878 <1410497@iub.edu.bd>
2018-03-26 16:20:18 +0000

    Assignment 11

---
+++ firstasyncio.js
@@ -0,0 +1,7 @@
+var fs = require('fs');
+
+// read file
+var file = fs.readFile(process.argv[2], function(err, data) {	// the parameters are default 
+  var str = data.toString();	// convert buffer to string
+  console.log(str.split('\n').length - 1);  // count new lines
+});
---
+++ myfirstio.js
@@ -0,0 +1,9 @@
+
+var fs = require('fs');
+	
+var bf = fs.readFileSync(process.argv[2]); 
+
+var str = bf.toString(); 	
+
+
+console.log(str.split('\n').length - 1);
